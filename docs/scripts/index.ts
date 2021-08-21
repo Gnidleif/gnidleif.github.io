1ba83e2dc8a7e07eb74d597522ed57adace74ed2
Gnidleif <gnidleif@gmail.com>
2021-08-21 13:30:33 +0200

    Not enough lorem ipsum

--- docs/scripts/index.ts
+++ docs/scripts/index.ts
@@ -59,6 +59,6 @@ class Fester implements TextGenerator {
 
     generate(message: string): string {
         let text: string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-        return text.repeat(Math.floor(Math.random() * 10));
+        return text.repeat(Math.floor(Math.random() * 100));
     }
 }
