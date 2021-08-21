82aaaae4a4c244227bca8a4ee428adf53946accd
Gnidleif <gnidleif@gmail.com>
2021-08-21 13:26:40 +0200

    Implemented actual stuff

--- docs/scripts/index.js
+++ docs/scripts/index.js
@@ -1,10 +1,50 @@
 "use strict";
-var GillmanGenerator = /** @class */ (function () {
-    function GillmanGenerator(message) {
-        this.test = message;
+class Index {
+    constructor() {
+        this.modes = {};
+        this.modes["spongebob"] = new Spongebob();
+        this.modes["fester"] = new Fester();
+        this.InitHTML();
     }
-    GillmanGenerator.prototype.greet = function () {
-        return "The message: " + this.test;
-    };
-    return GillmanGenerator;
-}());
+    InitHTML() {
+        $("#input_button").on("click", () => {
+            let key = $("#select_mode :selected").val();
+            let message = $("#input_text").val();
+            if (message.length > 0) {
+                $("#output_text").val(this.modes[key].generate(message));
+            }
+            else {
+                $("#output_text").val("");
+            }
+        });
+        $("#output_button").on("click", () => {
+            let generated = $("#output_text").val();
+            if (generated.length > 0) {
+                $("#output_text").trigger("select");
+                document.execCommand("copy");
+            }
+        });
+        var dropdown = $("#select_mode");
+        for (let key in this.modes) {
+            dropdown.append("<option value='" + key + "'>" + key.charAt(0).toUpperCase() + key.slice(1) + "</option>");
+        }
+    }
+    SpongeBob(message) {
+        return message.split('').map((chr) => Math.floor(Math.random() * 2) % 2 === 0 ? chr.toUpperCase() : chr.toLowerCase()).join('');
+    }
+}
+class Spongebob {
+    constructor() {
+    }
+    generate(message) {
+        return message.split('').map((chr) => Math.floor(Math.random() * 2) % 2 === 0 ? chr.toUpperCase() : chr.toLowerCase()).join('');
+    }
+}
+class Fester {
+    constructor() {
+    }
+    generate(message) {
+        let text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+        return text.repeat(Math.floor(Math.random() * 10));
+    }
+}
--- docs/scripts/index.ts
+++ docs/scripts/index.ts
@@ -1,11 +1,64 @@
-class GillmanGenerator {
-    test: string;
+class Index {
+    modes: { [key: string]: TextGenerator };
 
-    constructor(message: string) {
-        this.test = message;
+    constructor() {
+        this.modes = {};
+        this.modes["spongebob"] = new Spongebob();
+        this.modes["fester"] = new Fester();
+
+        this.InitHTML();
     }
 
-    greet() {
-        return "The message: " + this.test;
+    InitHTML(): void {
+        $("#input_button").on("click", () => {
+            let key: string = $("#select_mode :selected").val() as string;
+            let message: string = $("#input_text").val() as string;
+            if (message.length > 0) {
+                $("#output_text").val(this.modes[key].generate(message));
+            }
+            else {
+                $("#output_text").val("");
+            }
+        });
+
+        $("#output_button").on("click", () => {
+            let generated: string = $("#output_text").val() as string;
+            if (generated.length > 0) {
+                $("#output_text").trigger("select");
+                document.execCommand("copy");
+            }
+        });
+
+        var dropdown = $("#select_mode");
+        for (let key in this.modes) {
+            dropdown.append("<option value='" + key + "'>" + key.charAt(0).toUpperCase() + key.slice(1) + "</option>");
+        }
+    }
+
+    SpongeBob(message: string): string {
+        return message.split('').map((chr) => Math.floor(Math.random() * 2) % 2 === 0 ? chr.toUpperCase() : chr.toLowerCase()).join('');
+    }
+}
+
+interface TextGenerator {
+    generate(message: string): string;
+}
+
+class Spongebob implements TextGenerator {
+    constructor() {
+    }
+
+    generate(message: string): string {
+        return message.split('').map((chr) => Math.floor(Math.random() * 2) % 2 === 0 ? chr.toUpperCase() : chr.toLowerCase()).join('');
+    }
+}
+
+class Fester implements TextGenerator {
+    constructor() {
+    }
+
+    generate(message: string): string {
+        let text: string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+        return text.repeat(Math.floor(Math.random() * 10));
     }
 }
